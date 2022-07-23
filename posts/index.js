247aefe7e4b0f00ab06f29badad7888cf2bec60b
Denver <70090559+Denver44@users.noreply.github.com>
2022-07-23 14:07:45 +0530

    22. Handling CORS Errors

--- posts/index.js
+++ posts/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { randomBytes } from 'crypto';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
