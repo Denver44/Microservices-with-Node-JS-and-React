2bae318c7cce1931bd97a40f6e1fede7e4c1a299
Denver <70090559+Denver44@users.noreply.github.com>
2024-01-14 22:36:22 +0530

    Connecting to MongoDB

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -23,6 +24,19 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!');
-});
+const start = async () => {
+  try {
+    // To connect with the cluster Ip address
+    // our cluster domain name is auth-mongo-srv
+    // and the port is 27017
+    // We are using auth as the database name auth
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
+start();
